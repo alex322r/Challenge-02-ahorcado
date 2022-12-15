26edfeed295c179f3fdd86accdaf287500cc85ca
alexis <alexis@alexis>
2022-12-15 15:50:24 -0500

    final

--- ahorcado.js
+++ ahorcado.js
@@ -1,79 +1,223 @@
-let palabras = ["html","oracle"];
 
-let tablero = document.querySelector(".palabra-adivinar");
-let tablero2 = document.querySelector(".letras-usadas");
-let palabraSecreta = "";
-let letras = [];
-let errores = 8;
+const logo = document.querySelector("[logo]")
+const btnIniciarJuego = document.querySelector("[btn-start]");
+const mainElement = document.querySelector("[main-div]")
+const ahorcadoElement = document.querySelector(".ahorcado-div")
+const btnNuevoJUego = document.querySelector("[nuevo-juego]")
+const botones = document.querySelector(".botones")
+const btnRendirse = document.querySelector("[rendirse]")
+const btnNuevaPalabra = document.querySelector("[btn-add]")
+const munnecoElement = document.querySelector("[data-munneco]")
+const nuevaPalabraElement = document.querySelector("[agregar-palabra]")
+const nuevaPalabraTextarea = document.querySelector("[data-palabra]")
 
+const principal = document.querySelector(".botones")
 
-function escogerPalabraSecreta() {
-
-    let palabra = palabras[Math.floor(Math.random()*palabras.length)].toUpperCase();
-    palabraSecreta = palabra;
-    console.log(palabraSecreta);
-
-}
-
-function comprobarLetra(key) {
-    let estado =false;
-    if(key >=65 && letras.indexOf(key) || key <= 90 && letras.indexOf(key)){
-        letras.push(key)
-        console.log(key)
-        return estado
-
-    }else { 
-        estado = true
-        console.log(key)
-        return estado
-    }
+btnRendirse.addEventListener("click", ()=> {
+   if(confirm("Esta seguro que quieres rendirte")) {
+    ahorcadoElement.style.display= "none"; 
+    botones.style.display= "flex";
+    palabraAdivinar.innerHTML = "";
+    letrasAcertadasElement.innerHTML = "";
+    letrasErradasElement.innerHTML = "";
+    munnecoElement.innerHTML= "";
+   }
     
-}
+})
 
- function dibujarEspacios(letra) {
-     for(let i=0;i<palabraSecreta.length;i++){
-         let item = document.createElement("SPAN");
-         let textItem = document.createTextNode("");
-         
-         item.appendChild(textItem);
-         tablero.appendChild(item);
-         item.classList.add("letra-adivinada");
-         
-
-     }
-    
- }
-
- 
-
-//  function anadirLetraIncorrecta() {
-//     errores=errores-1;
-//     
-    
-//  }
-
+btnIniciarJuego.addEventListener("click", iniciarJuego)
 
 function iniciarJuego() {
+    btnIniciarJuego.parentElement.style.display = "none";
+    ahorcadoElement.style.display= "block";
+    logo.classList.replace("logo","logo-naranja");    
+    btnNuevoJUego.removeAttribute.disabled;
+    btnNuevoJUego.classList.add("juego-activo")
+    //nuevoJuego();
+    activarTeclas();    
+}
 
-    escogerPalabraSecreta();
-    dibujarEspacios();
-    document.onkeydown = (e)=> {
-        let letra = e.key.toUpperCase();
-        
-        
-        if(comprobarLetra(letra) && palabraSecreta.includes(letra)){
-            for(let i=0 ; i<palabraSecreta.length; i++){
-                if(palabraSecreta[i]=== letra){
-                    
-                }
-            }
-        } 
+btnNuevaPalabra.addEventListener("click", ()=> {
+    btnIniciarJuego.parentElement.style.display = "none";
+    nuevaPalabraElement.style.display = "flex"
+    logo.classList.replace("logo","logo-violeta");  
+})
+
+btnNuevoJUego.addEventListener("click", () => {
+    btnNuevoJUego.disabled="true";
+    btnNuevoJUego.classList.remove("juego-activo");
+    nuevoJuego();
+    activarTeclas()
+})
+
+
+
+const palabras = ["alexis"];
+const palabraAdivinar = document.querySelector("[palabra]");
+const letrasAcertadasElement = document.querySelector("[letras-acertadas]");
+const letrasErradasElement = document.querySelector("[letras-erradas]");
+
+let palabraSecreta = palabras[Math.floor(Math.random()*palabras.length)]; 
+let letrasAcertadas = [];
+let  letrasErradas = [];
+let vidas = 7;
+let contador = palabraSecreta.length;
+let aciertos = 0;
+let palabraSecretaLetrasUnicas = 0;
+
+
+
+function nuevoJuego(){
+    palabraSecreta = palabras[Math.floor(Math.random()*palabras.length)]; 
+    letrasAcertadas = [];
+    letrasErradas = [];
+    vidas = 7;
+    palabraSecretaLetrasUnicas = new Set(palabraSecreta).size;
+    
+    contador = palabraSecreta.length;
+    aciertos = 0;
+    avisoElement.innerText= "";
+    palabraAdivinar.innerHTML = "";
+    letrasAcertadasElement.innerHTML = "";
+    letrasErradasElement.innerHTML = "";
+    munnecoElement.innerHTML= "";
+    dibujarPalabraSecreta();
+    dibujarHorca();
+}
+
+function dibujarPalabraSecreta() {
+    for (let letra of palabraSecreta) {
+        const spanLetra = document.createElement("span")      
+        if (letrasAcertadas.includes(letra)){
+            spanLetra.innerText = letra.toLocaleUpperCase();
+            contador--
+        } else {
+            spanLetra.innerText = " ";
+        }
+        palabraAdivinar.appendChild(spanLetra);
     }
+}
+
+function comprobarEstado() {
+    if (aciertos  == palabraSecretaLetrasUnicas) {
+        AvisoGanarPerder("GANASTE!")
+        btnNuevoJUego.removeAttribute("disabled");
+        document.removeEventListener("keydown", presionarTecla)   
+        btnNuevoJUego.classList.add("juego-activo") 
+    }
+
+    if (vidas == 0) {
+        AvisoGanarPerder("PERDISTE!")
+        btnNuevoJUego.removeAttribute("disabled") ;
+        document.removeEventListener("keydown", presionarTecla)   
+        btnNuevoJUego.classList.add("juego-activo") 
+    }
+}
+
+function activarTeclas(){
+    document.addEventListener("keydown", presionarTecla)  
+}  
+
+function presionarTecla(e) {
+
+    if (e.keyCode >= 64 && e.keyCode<=90 || e.keyCode ==192) {  
+        if (palabraSecreta.includes(e.key) && !letrasAcertadas.includes(e.key)) {
+            letrasAcertadas.push(e.key);
+            const spanLetraAcertada = document.createElement("span")
+            console.log(e.key)
+            spanLetraAcertada.innerText= e.key;
+            letrasAcertadasElement.appendChild(spanLetraAcertada);
+            aciertos++;
+                
+        }else if (!palabraSecreta.includes(e.key)&& !letrasErradas.includes(e.key)) {
+            letrasErradas.push(e.key);
+            const spanLetraErrada = document.createElement("span");
+            spanLetraErrada.innerText= e.key;
+            letrasErradasElement.appendChild(spanLetraErrada);
+            vidas--
+            munnecoElement.appendChild(dibujarMunneco());  
+        }     
+        console.log(palabraSecreta.length, aciertos)
+        palabraAdivinar.innerHTML = "";
+        dibujarPalabraSecreta();               
+    } 
+    comprobarEstado();    
     
 }
 
-// function escribirLetraCorrecta(index) {
-    // tablero.
+function dibujarMunneco() { 
+    const munneco = document.createElement("div");
+    munneco.classList.add(`munneco-parte`);
+    munneco.classList.add(`munneco-parte${vidas}`);
+    return munneco
+}
+
+palabrasNuevas = JSON.parse(localStorage.getItem("palabras"))  || [];
+
+
+
+function agregarpalabraLocal(){
+    if (!palabrasNuevas.includes(nuevaPalabraTextarea.value.toLowerCase())){
+        palabrasNuevas.push(nuevaPalabraTextarea.value.toLowerCase())
+        nuevaPalabraTextarea.value= "";
+        console.log(palabrasNuevas)
+        localStorage.setItem("palabras", JSON.stringify(palabrasNuevas))
+        console.log(JSON.stringify(palabrasNuevas))
+        palabrasNuevas.forEach(palabraNueva => {
+            palabras.push(palabraNueva);
+        });
+        iniciarJuego()
+        nuevoJuego()
+    } else {
+        alert("esa palabra ya existe")
+    }
+} 
+
+nuevaPalabraTextarea.addEventListener("keydown", (event)=> {
+    if(!((event.keyCode >= 65) && (event.keyCode <= 90) || (event.keyCode >= 97) && (event.keyCode <= 122) || (event.keyCode >= 48) && (event.keyCode <= 57) || event.keyCode==8)){
+        event.returnValue = false;
+        alert("No se permiten caracteres especiales")
+        return;
+     }
+     event.returnValue = true;
+})
     
-// }
-iniciarJuego()
+
+
+const btnAddStart = document.querySelector("[add-start]");
+
+
+const btnCancel = document.querySelector("[btn-cancel]")
+
+btnCancel.addEventListener("click", ()=> {
+
+    logo.classList.replace("logo-violeta", "logo")
+
+    nuevaPalabraElement.style.display="none";
+    botones.style.display="flex";
+    
+    
+})
+
+btnAddStart.addEventListener("click", agregarpalabraLocal)
+
+
+function dibujarHorca() {
+    const piso = document.createElement("div");
+    piso.classList.add("piso")
+    const poste = document.createElement("div");
+    poste.classList.add("poste")
+    const poste2 = document.createElement("div");
+    poste2.classList.add("poste2")
+    munnecoElement.appendChild(piso);
+    munnecoElement.appendChild(poste);
+    munnecoElement.appendChild(poste2);
+}
+
+const avisoElement = document.createElement("div");
+function AvisoGanarPerder(aviso){
+    
+    avisoElement.classList.add("aviso");
+    avisoElement.innerText=aviso;
+    ahorcadoElement.appendChild(avisoElement);
+}
